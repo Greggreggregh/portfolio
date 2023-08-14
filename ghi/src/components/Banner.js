11642785c8f1bd3decbdbad06460905722dc020a
Greg Herren <greg.herren@gmail.com>
2023-08-14 19:13:59 -0400

    improving banner for mobile

--- ghi/src/components/Banner.js
+++ ghi/src/components/Banner.js
@@ -4,6 +4,7 @@ import { ArrowRightCircle } from "react-bootstrap-icons"
 import headerImg from "../assets/img/greg-waving-memoji.png"
 import { HashLink } from 'react-router-hash-link';
 import { BrowserRouter } from "react-router-dom";
+import { useMediaQuery } from 'react-responsive';
 
 export const Banner = () => {
     const [loopNum, setLoopNum] = useState(0);
@@ -11,6 +12,7 @@ export const Banner = () => {
     const [text, setText] = useState('');
     const [textLength, setTextLength] = useState(0);
     const [delta, setDelta] = useState(400 * Math.random());
+    const isMobile = useMediaQuery({ query: '(max-width: 991px)'});
     const period = 2000;
 
     useEffect(() => {
@@ -57,18 +59,30 @@ export const Banner = () => {
 
     return (
         <BrowserRouter>
-            <section className="banner" id="home">
+            <section className={isMobile ? "mobile-banner-image" : "d-none"} id={isMobile ? "home": ""}>
                 <Container>
                     <Row className="align-items-center">
-                        <Col md={7}>
-                            <span className="tagline">Welcome to my Portfolio</span>
-                            <h1>{`Hi! I'm Greg, `}<span className="wrap">{text}</span></h1>
-                            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                        <Col xs={12} className="small-img">
+                            <img src={headerImg} alt="Header Img" />
+                        </Col>
+                    </Row>
+                </Container>
+            </section>
+            <section className={isMobile ? "mobile-banner" : "banner"} id={isMobile ? "": "home"}>
+                <Container>
+                    <Row className="align-items-center">
+                        <Col xs={12} lg={8}>
+                            <h1>{`Hi👋`}&nbsp;&nbsp;{`I'm Greg,`}<br/><span className="wrap">{text}</span></h1>
+                            <p>A multi-faceted software engineer with a background in product and business. <br/><br/>I enjoy developing for web, mobile, and browser extensions. Check out{' '}
+                                <HashLink className="link" to='#projects'>
+                                    <button>my projects</button>
+                                </HashLink>!
+                            </p>
                             <HashLink to='#connect'>
                                 <button>Let's Connect <ArrowRightCircle size={25} /></button>
                             </HashLink>
                         </Col>
-                        <Col md={5} className="d-flex justify-content-center d-none d-md-block">
+                        <Col className={isMobile ? "d-none" : "align-self-start d-none d-md-block"}>
                             <img src={headerImg} alt="Header Img" />
                         </Col>
                     </Row>
--- ghi/src/components/NavBar.js
+++ ghi/src/components/NavBar.js
@@ -13,7 +13,7 @@ export const NavBar = () => {
   const [activeLink, setActiveLink] = useState('home');
   const [scrolled, setScrolled] = useState(false);
   const [expanded, setExpanded] = useState(false);
-  const isNavbarCollapsed = useMediaQuery({ query: '(max-width: 991px)'});
+  const isMobile = useMediaQuery({ query: '(max-width: 991px)'});
 
   useEffect(() => {
     const onScroll = () => {
@@ -29,39 +29,54 @@ export const NavBar = () => {
     return () => window.removeEventListener("scroll", onScroll);
   }, [])
 
+  useEffect(() => {
+    const onDocumentClick = (e) => {
+      const container = document.getElementById("navbar")
+      if (e.target !== container && !container.contains(e.target)) {
+        setExpanded(false)
+      }
+    }
+
+    document.addEventListener("click", onDocumentClick);
+
+return () => document.removeEventListener("click", onDocumentClick);
+  }, [])
+
   const onUpdateActiveLink = (value) => {
     setActiveLink(value);
   }
 
   return (
-    <BrowserRouter>
-      <Navbar expand="lg" className={scrolled || isNavbarCollapsed ? "scrolled" : ""} expanded={expanded}>
-        <Container>
-          <Navbar.Brand href="#home">
-            <img src={logo} style={{height: "55px"}} alt="Logo" />
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" onClick={() => setExpanded(expanded ? false : "expanded")}/>
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="ms-auto">
-              <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link text-end' : 'navbar-link text-end'} onClick={() => {onUpdateActiveLink('home'); setExpanded(false);}}>Home</Nav.Link>
-              <Nav.Link href="#skills" className={activeLink === 'skills' ? 'active navbar-link text-end' : 'navbar-link text-end'} onClick={() => {onUpdateActiveLink('skills'); setExpanded(false);}}>Skills</Nav.Link>
-              <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link text-end' : 'navbar-link text-end'} onClick={() => {onUpdateActiveLink('projects'); setExpanded(false);}}>Projects</Nav.Link>
-            </Nav>
-            <span className="navbar-text justify-content-end">
-              <div className="social-icon text-md-end">
-                <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/greg-herren/"><img src={linkedinIcon} alt="" /></a>
-                <a target="_blank" rel="noreferrer" href="https://github.com/Greggreggregh"><img src={githubIcon} alt="" /></a>
-                <a target="_blank" rel="noreferrer" href="https://gitlab.com/greg-herren"><img src={gitlabIcon} alt="" /></a>
-              </div>
-            </span>
-            <span className="navbar-text justify-content-end">
-              <HashLink to='#connect' onClick={()=>setExpanded(false)}>
-                <button className="vvd"><span>{`Let's Connect`}</span></button>
-              </HashLink>
-            </span>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-    </BrowserRouter>
+    <div id="navbar">
+      <BrowserRouter>
+        <Navbar expand="lg" className={scrolled || isMobile ? "scrolled" : ""} expanded={expanded}>
+          <Container>
+            <Navbar.Brand href="#home" onClick={()=>setExpanded(false)}>
+              <img src={logo} style={{height: "55px"}} alt="Logo" />
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" onClick={() => setExpanded(expanded ? false : "expanded")}/>
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="ms-auto">
+                <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link text-end' : 'navbar-link text-end'} onClick={() => {onUpdateActiveLink('home'); setExpanded(false);}}>Home</Nav.Link>
+                <Nav.Link href="#skills" className={activeLink === 'skills' ? 'active navbar-link text-end' : 'navbar-link text-end'} onClick={() => {onUpdateActiveLink('skills'); setExpanded(false);}}>Skills</Nav.Link>
+                <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link text-end' : 'navbar-link text-end'} onClick={() => {onUpdateActiveLink('projects'); setExpanded(false);}}>Projects</Nav.Link>
+              </Nav>
+              <span className="navbar-text justify-content-end">
+                <div className="social-icon text-md-end">
+                  <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/greg-herren/"><img src={linkedinIcon} alt="" /></a>
+                  <a target="_blank" rel="noreferrer" href="https://github.com/Greggreggregh"><img src={githubIcon} alt="" /></a>
+                  <a target="_blank" rel="noreferrer" href="https://gitlab.com/greg-herren"><img src={gitlabIcon} alt="" /></a>
+                </div>
+              </span>
+              <span className="navbar-text justify-content-end">
+                <HashLink to='#connect' onClick={()=>setExpanded(false)}>
+                  <button className="vvd"><span>{`Let's Connect`}</span></button>
+                </HashLink>
+              </span>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      </BrowserRouter>
+    </div>
   )
 }
