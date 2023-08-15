7beb975f744aea1feb4463686b73267c8e458c7e
Greg Herren <greg.herren@gmail.com>
2023-08-15 17:10:00 -0400

    building out skills carousel & adding skills grid

--- ghi/src/components/Skills.js
+++ ghi/src/components/Skills.js
@@ -1,23 +1,36 @@
+import { useState } from "react";
 import { Container, Row, Col } from "react-bootstrap"
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
-import meter1 from '../assets/img/meter1.svg';
-import meter2 from '../assets/img/meter2.svg';
-import meter3 from '../assets/img/meter3.svg';
+import bootstrap from '../assets/img/skills_bootstrap.svg';
+import css from '../assets/img/skills_css.svg';
+import django from '../assets/img/skills_django.svg';
+import docker from '../assets/img/skills_docker.png';
+import fastapi from '../assets/img/skills_fastapi.svg';
+import html5 from '../assets/img/skills_html5.svg';
+import javascript from '../assets/img/skills_javascript.png';
+import mongodb from '../assets/img/skills_mongodb.svg';
+import postgres from '../assets/img/skills_postgresql.svg';
+import python from '../assets/img/skills_python.png';
+import rabbitmq from '../assets/img/skills_rabbitmq.png';
+import react from '../assets/img/skills_react.svg';
+import redux from '../assets/img/skills_redux.svg';
 
 export const Skills = () => {
+
+    const [expanded, setExpanded] = useState(false);
+
     const responsive = {
         superLargeDesktop: {
-          // the naming can be any, depends on you.
-          breakpoint: { max: 4000, min: 3000 },
-          items: 5
+          breakpoint: { max: 4000, min: 992 },
+          items: 4
         },
         desktop: {
-          breakpoint: { max: 3000, min: 1024 },
+          breakpoint: { max: 991, min: 768 },
           items: 3
         },
         tablet: {
-          breakpoint: { max: 1024, min: 464 },
+          breakpoint: { max: 767, min: 464 },
           items: 2
         },
         mobile: {
@@ -25,7 +38,8 @@ export const Skills = () => {
           items: 1
         }
       };
-    return (
+
+      return (
         <section className="skill" id="skills">
             <Container>
                 <Row>
@@ -34,24 +48,141 @@ export const Skills = () => {
                             <h2>
                                 Skills
                             </h2>
-                            <Carousel responsive={responsive} infinite={true} className="skill-slider">
+                            <button onClick={() => {setExpanded(expanded ? false : true)}}>{expanded ? "▿ show preview" : "▹ show all"}</button>
+                            <Carousel className={expanded ? "d-none" : "skill-slider"} responsive={responsive} infinite={true} swipeable={true} draggable={true} arrows={false} autoPlay={true} autoPlaySpeed={2000}>
                                 <div className="item">
-                                    <img src={meter1} alt="" />
-                                    <h5>Full Stack Development</h5>
+                                    <img src={python} alt="Python logo" />
+                                    <h5>Python</h5>
                                 </div>
                                 <div className="item">
-                                    <img src={meter2} alt="" />
-                                    <h5>Web Development</h5>
+                                    <img src={javascript} alt="" />
+                                    <h5>Javascript</h5>
                                 </div>
                                 <div className="item">
-                                    <img src={meter3} alt="" />
-                                    <h5>Product Management</h5>
+                                    <img src={react} alt="" />
+                                    <h5>React</h5>
                                 </div>
                                 <div className="item">
-                                    <img src={meter1} alt="" />
-                                    <h5>Business Analysis</h5>
+                                    <img className="logo-bg" src={django} alt="" />
+                                    <h5>Django</h5>
+                                </div>
+                                <div className="item">
+                                    <img src={fastapi} alt="" />
+                                    <h5>FastAPI</h5>
+                                </div>
+                                <div className="item">
+                                    <img src={html5} alt="" />
+                                    <h5>HTML5</h5>
+                                </div>
+                                <div className="item">
+                                    <img src={css} alt="" />
+                                    <h5>CSS</h5>
+                                </div>
+                                <div className="item">
+                                    <img src={bootstrap} alt="" />
+                                    <h5>Bootstrap</h5>
+                                </div>
+                                <div className="item">
+                                    <img src={docker} alt="" />
+                                    <h5>Docker</h5>
+                                </div>
+                                <div className="item">
+                                    <img src={postgres} alt="" />
+                                    <h5>PostgreSQL</h5>
+                                </div>
+                                <div className="item">
+                                    <img src={mongodb} alt="" />
+                                    <h5>MongoDB</h5>
+                                </div>
+                                <div className="item">
+                                    <img src={redux} alt="" />
+                                    <h5>Redux</h5>
+                                </div>
+                                <div className="item">
+                                    <img src={rabbitmq} alt="" />
+                                    <h5>RabbitMQ</h5>
                                 </div>
                             </Carousel>
+                            <Row className={expanded ? "skill-grid" : "d-none"}>
+                                <Col xs={12} sm={6} md={4} lg={3}>
+                                    <div className="item">
+                                        <img src={python} alt="Python logo" />
+                                        <h5>Python</h5>
+                                    </div>
+                                </Col>
+                                <Col xs={12} sm={6} md={4} lg={3}>
+                                    <div className="item">
+                                        <img src={javascript} alt="" />
+                                        <h5>Javascript</h5>
+                                    </div>
+                                </Col>
+                                <Col xs={12} sm={6} md={4} lg={3}>
+                                    <div className="item">
+                                        <img src={react} alt="" />
+                                        <h5>React</h5>
+                                    </div>
+                                </Col>
+                                <Col xs={12} sm={6} md={4} lg={3}>
+                                    <div className="item">
+                                        <img className="logo-bg" src={django} alt="" />
+                                        <h5>Django</h5>
+                                    </div>
+                                </Col>
+                                <Col xs={12} sm={6} md={4} lg={3}>
+                                    <div className="item">
+                                        <img src={fastapi} alt="" />
+                                        <h5>FastAPI</h5>
+                                    </div>
+                                </Col>
+                                <Col xs={12} sm={6} md={4} lg={3}>
+                                    <div className="item">
+                                        <img src={html5} alt="" />
+                                        <h5>HTML5</h5>
+                                    </div>
+                                </Col>
+                                <Col xs={12} sm={6} md={4} lg={3}>
+                                    <div className="item">
+                                        <img src={css} alt="" />
+                                        <h5>CSS</h5>
+                                    </div>
+                                </Col>
+                                <Col xs={12} sm={6} md={4} lg={3}>
+                                    <div className="item">
+                                        <img src={bootstrap} alt="" />
+                                        <h5>Bootstrap</h5>
+                                    </div>
+                                </Col>
+                                <Col xs={12} sm={6} md={4} lg={3}>
+                                    <div className="item">
+                                        <img src={docker} alt="" />
+                                        <h5>Docker</h5>
+                                    </div>
+                                </Col>
+                                <Col xs={12} sm={6} md={4} lg={3}>
+                                    <div className="item">
+                                        <img src={postgres} alt="" />
+                                        <h5>PostgreSQL</h5>
+                                    </div>
+                                </Col>
+                                <Col xs={12} sm={6} md={4} lg={3}>
+                                    <div className="item">
+                                        <img src={mongodb} alt="" />
+                                        <h5>MongoDB</h5>
+                                    </div>
+                                </Col>
+                                <Col xs={12} sm={6} md={4} lg={3}>
+                                    <div className="item">
+                                        <img src={redux} alt="" />
+                                        <h5>Redux</h5>
+                                    </div>
+                                </Col>
+                                <Col xs={12} sm={6} md={4} lg={3}>
+                                    <div className="item">
+                                        <img src={rabbitmq} alt="" />
+                                        <h5>RabbitMQ</h5>
+                                    </div>
+                                </Col>
+                            </Row>
                             <p>Something missing? Don't worry, I'm a quick study 🤓</p>
                         </div>
                     </Col>
