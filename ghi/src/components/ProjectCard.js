8b01002392b0b1635b13843b49ca1ff50f635b72
Greg Herren <greg.herren@gmail.com>
2023-08-24 18:03:27 -0400

    moving content out of hover box over project gif

--- ghi/src/components/ProjectCard.js
+++ ghi/src/components/ProjectCard.js
@@ -3,18 +3,17 @@ import { Col } from "react-bootstrap"
 export const ProjectCard = ({ title, subtitle, description, imgUrl, codeUrl, appUrl }) => {
     return (
         <Col lg={6}>
+            <h3 className="text-center">{title}</h3>
+            <h5 className="text-center">〈〈 {subtitle} 〉〉</h5>
+            <div className="text-center">
+                {codeUrl && <button onClick = {() => {window.open(codeUrl, '_blank')}}><span>Code</span></button>}
+                {appUrl && <button onClick = {() => {window.open(appUrl, '_blank')}}><span>App</span></button>}
+            </div>
             <div className="proj-imgbx">
                 <img src={imgUrl} alt="Gif showing a brief demo of the service"/>
                 <div className="proj-txtx">
-                    <h3>{title}</h3>
-                    <h5>{subtitle}</h5>
+                    <h5>Made with:</h5>
                     <span>{description}</span>
-                    <div>
-                        <div>
-                            {codeUrl && <button onClick = {() => {window.open(codeUrl, '_blank')}}><span>Code</span></button>}
-                            {appUrl && <button onClick = {() => {window.open(appUrl, '_blank')}}><span>App</span></button>}
-                        </div>
-                    </div>
                 </div>
             </div>
         </Col>
